4747e791d8648543f50236751dec7448b123e9c1
Robert Pietrusinski <robert.x.pietrusinski@gsk.com>
2024-09-09 16:29:07 +0200

    Refactor

--- src/handlers.rs
+++ src/handlers.rs
@@ -13,23 +13,27 @@ use std::sync::{Arc, Mutex};
 
 ///Return arbitrary json object. `serde_json::Value` can be a valid json object of any kind.
 ///Wrapping output around `axum::Json` as it implements `IntoResponse` trait.
-pub async fn get_json(State(s): State<Arc<Mutex<AppState>>>) -> Json<Value> {
-    let mut binding = s.lock().unwrap();
-    binding.increment();
+pub async fn get_json(State(s): State<Arc<Mutex<AppState>>>) -> Result<Json<Value>, StatusCode> {
+    match s.lock() {
+        Ok(mut x) => x.increment(),
+        Err(_e) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
 
-    Json(json!({
+    Ok(Json(json!({
         "data": 42,
         "fields": ["a", "b", "c"],
         "X": {"Y": 1, "z": 2},
         "float_val": "1.34",
-    }))
+    })))
 }
 
 ///Return how many times `get_json` has been called. This introduces shared state implemented with
 /// Arc and Mutex.
-pub async fn get_json_counter<'a>(State(s): State<Arc<Mutex<AppState>>>) -> String {
-    let binding = s.lock().unwrap();
-    binding.get_counter().to_string()
+pub async fn get_json_counter(State(s): State<Arc<Mutex<AppState>>>) -> Result<String, StatusCode> {
+    match s.lock() {
+        Ok(x) => Ok(x.get_counter().to_string()),
+        Err(_e) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
 }
 
 ///Provide request json in form of `RequestJson` and outputs `ResponseJson`. Inside function
